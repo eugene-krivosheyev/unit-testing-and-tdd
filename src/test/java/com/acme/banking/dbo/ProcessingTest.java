e6a1372b75033153b8802e3450e80f524ea7cb04
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-06-25 17:23:04 +0300

    Add Fixture Builder demo.

--- src/test/java/com/acme/banking/dbo/ProcessingTest.java
+++ src/test/java/com/acme/banking/dbo/ProcessingTest.java
@@ -1,8 +1,9 @@
 package com.acme.banking.dbo;
 
+import com.acme.banking.dbo.repo.AccountRepository;
 import com.acme.banking.dbo.service.Processing;
 import com.acme.banking.dbo.service.TxLog;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.UUID;
 
@@ -12,15 +13,42 @@ import static org.mockito.Mockito.*;
 public class ProcessingTest {
     private static final double ANY_AMOUNT = 1.0;
     private static final UUID ANY_UUID = UUID.randomUUID();
+    private TxLog mockTxLog;
+    private Processing sut;
+    private AccountRepository accounts;
+
+    @BeforeClass @AfterClass
+    public static void setUpOnce() {
+
+    }
+
+    @Before @After
+    public void setUp() {
+        mockTxLog = mock(TxLog.class);
+        accounts = mock(AccountRepository.class);
+        sut = new Processing(mockTxLog);
+    }
 
     @Test
     public void shouldLogTxWithTxLogWhenCacheOperation() {
-        TxLog mockTxLog = mock(TxLog.class);
-        Processing sut = new Processing(mockTxLog);
+        AccountRepository stubRepo =
+            new FakeDatabaseAccountRepositoryBuilder()
+                .withAccount(
+                        new AccountBuilder()
+                                .withId()
+                                .withAmount()
+                            .build())
+                .withAccount(
+                        new AccountBuilder()
+                                .withId()
+                                .withClient() //defaults!
+                            .build())
+            .build();
 
         sut.cash(ANY_AMOUNT, ANY_UUID);
 
         verify(mockTxLog, times(1))
                 .log(anyDouble(), any(UUID.class));
     }
+
 }
