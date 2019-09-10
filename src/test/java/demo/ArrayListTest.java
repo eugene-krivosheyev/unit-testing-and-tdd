a145e14718ad2c7f7de811620dc92920a4543ef3
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-09-10 16:57:35 +0300

    Add guard clauses demo.

--- src/test/java/demo/ArrayListTest.java
+++ src/test/java/demo/ArrayListTest.java
@@ -1,23 +1,41 @@
 package demo;
 
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
 /**
  * Test-Case
  */
 public class ArrayListTest {
-    @Test //BDD = DDD к тестам
+    private ArrayList<Object> sut;
+    @Rule public final ExpectedException exception = ExpectedException.none();
+
+    @BeforeClass
+    public static void setUpGlobal() {
+
+    }
+
+    @Before
+    public void setUp() {
+        sut = new ArrayList<>();
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test (timeout = 10_000) //BDD = DDD к тестам
     public void shouldSizeIncrementedAndElementContainedWhenElementAdded() {
         //region Fixture | Arrange | Given
-        final ArrayList<Object> sut = new ArrayList<>();
         final Object dummy = new Object();
         assumeTrue(sut.size() == 0);
         //endregion
@@ -30,6 +48,19 @@ public class ArrayListTest {
         assertEquals(1, sut.size());
         assertTrue(sut.contains(dummy));
         //endregion
+    }
+
+
+    @Test
+    public void shouldSaveWhenAddNull() {
+        final ArrayList<Object> sut = new ArrayList<>();
+        sut.add(null);
+        assertEquals(1, sut.size());
+        assertTrue(sut.contains(null));
+
+        Assertions.assertThat("")
+                .contains("")
+                .isEmpty();
     }
 
 }
