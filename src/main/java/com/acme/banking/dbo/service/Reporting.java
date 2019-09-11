b65014ad598ef0cca322a745244c3362242c397d
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-09-11 10:56:41 +0300

    Add testability demo: Creator DP.

--- src/main/java/com/acme/banking/dbo/service/Reporting.java
+++ src/main/java/com/acme/banking/dbo/service/Reporting.java
@@ -1,12 +1,22 @@
 package com.acme.banking.dbo.service;
 
+import com.acme.banking.dbo.dal.AccountNotFoundException;
+import com.acme.banking.dbo.dal.AccountRepository;
+import com.acme.banking.dbo.domain.Account;
 import com.acme.banking.dbo.domain.Branch;
 
 public class Reporting {
+    private AccountRepository accounts = new JdbcAccountRepo(); //Creator [GRASP]
+
     /**
      * @return Markdown report for all branches, clients, accounts
      */
     public String getReport(Branch rootBranch) {
         return null;
+    }
+
+    public String getReport(Account account) throws AccountNotFoundException {
+        final Account foundAccount = accounts.findById(account.getId());
+        return "## " + foundAccount.getId() + " " + foundAccount.getClientId();
     }
 }
