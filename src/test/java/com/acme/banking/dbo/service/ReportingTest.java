925d231cb59b6b66a78879302ff88a6530681c69
BELITSKY Ivan <Ivan.BELITSKY@raiffeisen.ru>
2019-11-21 17:29:51 +0300

    TDD iteration 3 - RED

--- src/test/java/com/acme/banking/dbo/service/ReportingTest.java
+++ src/test/java/com/acme/banking/dbo/service/ReportingTest.java
@@ -1,6 +1,10 @@
 package com.acme.banking.dbo.service;
+import com.acme.banking.dbo.domain.Account;
 import com.acme.banking.dbo.domain.Branch;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -11,8 +15,13 @@ public class ReportingTest {
     public void shouldGetReportWhenBranchNotEmpty(){
         Reporting sut = new Reporting();
         Branch stubBranch= mock(Branch.class);
+        Collection<Account> stubAccounts =new ArrayList<>();
+        final Account stubAccount = mock(Account.class);
+        stubAccounts.add(stubAccount);
+        when(stubBranch.getAccounts()).thenReturn(stubAccounts);
         String report  = sut.getReport(stubBranch);
-        assertThat(report).contains("## clientName");
+
+        assertThat(report).contains("## "+stubAccount.getId());
 
     }
     @Test
