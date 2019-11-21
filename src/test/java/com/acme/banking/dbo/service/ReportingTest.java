f5aba5d299c6d967fb37a1121e218ec595fecd8b
BELITSKY Ivan <Ivan.BELITSKY@raiffeisen.ru>
2019-11-21 17:12:48 +0300

    TDD iteration 1 - GREEN

---
+++ src/test/java/com/acme/banking/dbo/service/ReportingTest.java
@@ -0,0 +1,16 @@
+package com.acme.banking.dbo.service;
+import com.acme.banking.dbo.domain.Branch;
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class ReportingTest {
+    @Test
+    public void shouldGetReportWhenBranchNotEmpty(){
+        Reporting sut = new Reporting();
+        Branch stubBranch= mock(Branch.class);
+        String report  = sut.getReport(stubBranch);
+        assertThat(report).contains("# BrunchName");
+    }
+}
