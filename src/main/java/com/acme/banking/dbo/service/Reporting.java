26b204274241148aad2bc26a2c544a18cb59e38c
BELITSKY Ivan <Ivan.BELITSKY@raiffeisen.ru>
2019-11-22 12:41:38 +0300

    TDD iteration 4 - RED

--- src/main/java/com/acme/banking/dbo/service/Reporting.java
+++ src/main/java/com/acme/banking/dbo/service/Reporting.java
@@ -7,7 +7,10 @@ public class Reporting {
      * @return Markdown report for all branches, clients, accounts
      */
     public String getReport(Branch rootBranch) {
-        String report = "# " + rootBranch.getName();
+
+        String reportName = rootBranch.getName();
+        if (reportName == null) reportName = "";
+        String report = "# " + reportName;
 
         if (!rootBranch.getAccounts().isEmpty()) {
             report += report + "## clientName";
