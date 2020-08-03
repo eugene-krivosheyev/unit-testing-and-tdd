4ef0e03d898b69dc61271de903018ed09b99a94f
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-08-03 14:00:52 +0300

    Add simple test demo.

--- src/test/java/demo/ArrayListTest.java
+++ src/test/java/demo/ArrayListTest.java
@@ -1,10 +1,28 @@
 package demo;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import static org.junit.Assert.*;
 
 public class ArrayListTest {
     @Test //BDD = DDD(test)
-    public void shouldGetElementAndSizeIncrementedWhenAddElement() {
+    public void shouldSizeIncrementedWhenAddElement() {
+        //region Fixture | Arrange | Given
+        final ArrayList<Object> sut = new ArrayList<>();
+        final Object dummy = new Object();
+        //endregion
 
+        //region Act | When
+        sut.add(dummy);
+        //endregion
+
+        //region Assert | Then
+        assertEquals("", 1, sut.size());
+        assertFalse("", sut.isEmpty());
+        //endregion
     }
 }
