1a45eb0b4d1e14c99c55a74ccca3fcdf5a1b4c90
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-08-03 12:01:01 +0300

    Add test naming demo.

---
+++ src/test/java/demo/ArrayListTest.java
@@ -0,0 +1,10 @@
+package demo;
+
+import org.junit.Test;
+
+public class ArrayListTest {
+    @Test //BDD = DDD(test)
+    public void shouldGetElementAndSizeIncrementedWhenAddElement() {
+
+    }
+}
