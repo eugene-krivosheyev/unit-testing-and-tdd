0ca3465b67da6b54dc3bbb98dae41edbf88ebeda
YASHENKOV Aleksey <Aleksey.YASHENKOV@raiffeisen.ru>
2020-08-05 16:04:01 +0300

    Refactoring tests (Client + SaveAccount) #1

--- src/test/java/com/acme/banking/dbo/domain/ClientTest.java
+++ src/test/java/com/acme/banking/dbo/domain/ClientTest.java
@@ -1,6 +1,5 @@
 package com.acme.banking.dbo.domain;
 
-import com.acme.banking.dbo.domain.Client;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,57 +9,50 @@ import java.util.UUID;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.*;
 
 public class ClientTest {
+    public static final UUID CLIENT_UUID = UUID.fromString("75852e48-dd5e-4062-921d-bae2bd91a045");
+    public static final String DUMMY_NAME = "dummy name";
+
     @Test
-    public void shouldClientSavePropertiesWhenCreated() {
-        //region given
-        UUID stubId = UUID.randomUUID();
-        String name = "test";
-        //endregion
+    public void shouldSavePropertiesWhenClientCreated() {
+        Client sut = new Client(CLIENT_UUID, DUMMY_NAME);
 
-        //region when
-        Client sut = new Client(stubId, name);
-        //endregion
-
-        //region then
-        assertNotNull(sut.getId());
-        assertNotNull(sut.getName());
-        assertEquals(stubId, sut.getId());
-        assertEquals(name, sut.getName());
+        assertEquals(CLIENT_UUID, sut.getId());
+        assertEquals(DUMMY_NAME, sut.getName());
 //        assertThat(sut.getId(),
 //            allOf(
 //                equalTo(stubId),
 //                notNullValue()
 //        ));
-        //endregion
     }
 
     @Rule
     public final ExpectedException expectedException = ExpectedException.none();
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenClientIdIsNull() {
-        UUID stubId = null;
-        String name = "test";
-        Client sut = new Client(stubId, name);
+    @Test
+    public void shouldNotCreateClientWhenIdIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("UUID can`t be null");
+
+        Client sut = new Client(null, DUMMY_NAME);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenClientNameIsNull() {
-        UUID stubId = UUID.randomUUID();
-        String name = null;
-        Client sut = new Client(stubId, name);
+    @Test
+    public void shouldNotCreateClientWhenNameIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Name can`t be null or empty");
+
+        Client sut = new Client(CLIENT_UUID, null);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenClientNameIsEmpty() {
-        UUID stubId = UUID.randomUUID();
-        String name = " ";
-        Client sut = new Client(stubId, name);
+    @Test
+    public void shouldNotCreateClientWhenNameIsEmpty() {
+        expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Name can`t be null or empty");
+
+        Client sut = new Client(CLIENT_UUID, " ");
     }
 }
--- src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
+++ src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
@@ -1,7 +1,5 @@
 package com.acme.banking.dbo.domain;
 
-import com.acme.banking.dbo.domain.Client;
-import com.acme.banking.dbo.domain.SavingAccount;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -12,66 +10,47 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class SavingAccountTest {
+    public static final UUID CLIENT_UUID = UUID.fromString("75852e48-dd5e-4062-921d-bae2bd91a045");
+    public static final UUID ACCOUNT_UUID = UUID.fromString("c4bf82eb-b982-47bb-9df6-76a6ebc7b707");
+    public static final String DUMMY_NAME = "dummy name";
+    final Client client = new Client(CLIENT_UUID, DUMMY_NAME);
+    final double amount = 1;
+
     @Test
     public void shouldSaveAccountWhenGettingNotNullClientWithPositiveAmount() {
-        //region Fixture | Arrange | Given
-        UUID clientId = UUID.randomUUID();
-        UUID accountId = UUID.randomUUID();
-        Client client = new Client(clientId, "test");
-        double amount = 10;
-        //endregion
-        //region Assumptions check
+        SavingAccount sut = new SavingAccount(ACCOUNT_UUID, client, amount);
 
-        //endregion
-
-        //region Act | When
-        SavingAccount sut = new SavingAccount(accountId, client, amount);
-        //endregion
-
-        //region Assert | Then
         assertNotNull(sut.getClient());
         assertEquals(client, sut.getClient());
-        assertEquals(clientId, sut.getClientId());
-        assertEquals(accountId, sut.getId());
+        assertEquals(CLIENT_UUID, sut.getClientId());
+        assertEquals(ACCOUNT_UUID, sut.getId());
         assertEquals(amount, sut.getAmount(), 0.001);
-        //endregion
     }
 
     @Rule
     public final ExpectedException expectedException = ExpectedException.none();
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenSaveAccountIdIsNull() {
-        UUID clientId = UUID.randomUUID();
-        UUID accountId = null;
-        String name = "test";
-        double amount = 10;
-        Client client = new Client(clientId, name);
-        SavingAccount sut = new SavingAccount(accountId, client, amount);
-
+    @Test
+    public void shouldNotCreateSaveAccountWhenIdIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("UUID can`t be null");
+
+        SavingAccount sut = new SavingAccount(null, client, amount);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenSaveAccountClientIsNull() {
-        UUID accountId = UUID.randomUUID();
-        String name = "test";
-        double amount = 10;
-        Client client = null;
-        SavingAccount sut = new SavingAccount(accountId, client, amount);
-
+    @Test
+    public void shouldNotCreateSaveAccountWhenClientIsNull() {
+        expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Client can`t be null");
+
+        SavingAccount sut = new SavingAccount(ACCOUNT_UUID, null, amount);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowExceptionWhenSaveAccountAmountIsNegative() {
-        UUID clientId = UUID.randomUUID();
-        UUID accountId = UUID.randomUUID();
-        Client client = new Client(clientId, "test");
-        double amount = -1;
-
-        SavingAccount sut = new SavingAccount(accountId, client, amount);
-
+    @Test
+    public void shouldNotCreateSaveAccountWhenAmountIsNegative() {
+        expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Amount can`t be negative");
+
+        SavingAccount sut = new SavingAccount(ACCOUNT_UUID, client, -1);
     }
 }
