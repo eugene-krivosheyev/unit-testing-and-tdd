23f8f3a2ffab7c4fdc6b913be5bd0f1f5f0ed1d8
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-08-06 13:32:53 +0300

    Add spy demo.

--- src/test/java/com/acme/banking/dbo/ProcessingTest.java
+++ src/test/java/com/acme/banking/dbo/ProcessingTest.java
@@ -1,22 +1,25 @@
 package com.acme.banking.dbo;
 
 import com.acme.banking.dbo.domain.Account;
+import com.acme.banking.dbo.domain.SavingAccount;
 import com.acme.banking.dbo.service.Processing;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static com.sun.tools.doclint.Entity.times;
 import static org.mockito.Mockito.*;
 
 public class ProcessingTest {
-    @Test
+    @Test @Ignore
     public void shouldWithdrawAndDepositWhenTransfer() {
         final Processing sut = new Processing();
         final Account fromAccountMock = mock(Account.class);
-        final Account toAccountMock = mock(Account.class);
+        final Account toAccountSpy = spy(new SavingAccount(null, null, 0));
+        when(toAccountSpy.getId()).thenThrow(new IllegalStateException());
 
-        sut.transfer(1., fromAccountMock, toAccountMock);
+        sut.transfer(1., fromAccountMock, toAccountSpy);
 
         verify(fromAccountMock, times(1)).withdraw(1.);
-        verify(toAccountMock).deposit(anyDouble());
+        verify(toAccountSpy).deposit(anyDouble());
     }
 }
