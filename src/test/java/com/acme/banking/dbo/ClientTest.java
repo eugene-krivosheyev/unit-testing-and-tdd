e2cd59aee4eed21c91625a8bbe5d2b9157ff6981
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-10-26 02:08:04 +0300

    Add Hamcrest Bean Properties Matcher demo.

--- src/test/java/com/acme/banking/dbo/ClientTest.java
+++ src/test/java/com/acme/banking/dbo/ClientTest.java
@@ -5,27 +5,25 @@ import org.junit.Test;
 
 import java.util.UUID;
 
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 
 public class ClientTest {
-    @Test
-    public void shouldSavePropertiesWhenCreated() {
-        //region given
-        UUID stubId = UUID.randomUUID();
-        //endregion
+    public static final UUID ID_STUB = UUID.fromString("8fe9595d-de6e-4d07-bc56-dacdad16f5c2");
 
+    @Test
+    public void shouldStorePropertiesWhenCreated() {
         //region when
-        Client sut = new Client(stubId, "dummy client name");
+        Client sut = new Client(ID_STUB, "dummy client name");
         //endregion
 
         //region then
-        assertThat(sut.getId(),
+        assertThat(sut,
             allOf(
-                equalTo(stubId),
-                notNullValue()
+                hasProperty("id", notNullValue()),
+                hasProperty("id", equalTo(ID_STUB)),
+                hasProperty("name", is("dummy client name"))
         ));
         //endregion
     }
