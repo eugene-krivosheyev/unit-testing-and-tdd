216d861b79f097f005349b08c8dc68872fe9318a
Aristov Ivan <ivan.aristov@raiffeisen.ru>
2020-10-27 11:57:26 +0300

    Coding iteration #01, SavingAccountTests

--- src/test/java/com/acme/banking/dbo/SavingAccountTest.java
+++ src/test/java/com/acme/banking/dbo/SavingAccountTest.java
@@ -1,22 +1,25 @@
 package com.acme.banking.dbo;
 
 import com.acme.banking.dbo.domain.Client;
+import com.acme.banking.dbo.domain.SavingAccount;
 import org.junit.Test;
 
 import java.util.Collections;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.junit.Assert.assertThat;
 
-public class ClientTest {
+public class SavingAccountTest {
     private static final UUID ID_STUB = UUID.fromString("8fe9595d-de6e-4d07-bc56-dacdad16f5c2");
+    private static final Client CLIENT_STUB = new Client(ID_STUB, "dummy client name", Collections.emptyList());
+    private static final double AMOUNT_STUB = 0.1;
 
     @Test
     public void shouldStorePropertiesWhenCreated() {
         //region when
-        Client sut = new Client(ID_STUB, "dummy client name", Collections.emptyList());
+        SavingAccount sut = new SavingAccount(ID_STUB, CLIENT_STUB, AMOUNT_STUB);
         //endregion
 
         //region then
@@ -24,7 +27,9 @@ public class ClientTest {
                 allOf(
                         hasProperty("id", notNullValue()),
                         hasProperty("id", equalTo(ID_STUB)),
-                        hasProperty("name", is("dummy client name"))
+                        hasProperty("client", notNullValue()),
+                        hasProperty("client", equalTo(CLIENT_STUB)),
+                        hasProperty("amount", equalTo(AMOUNT_STUB))
                 ));
         //endregion
     }
@@ -32,22 +37,21 @@ public class ClientTest {
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowIllegalArgumentExceptionWhenIdIsNull() {
         //region when
-        Client sut = new Client(null, "dummy client name", Collections.emptyList());
+        SavingAccount sut = new SavingAccount(null, CLIENT_STUB, AMOUNT_STUB);
         //endregion
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowIllegalArgumentExceptionWhenNameIsNull() {
+    public void shouldThrowIllegalArgumentExceptionWhenClientIsNull() {
         //region when
-        Client sut = new Client(ID_STUB, null, Collections.emptyList());
+        SavingAccount sut = new SavingAccount(ID_STUB, null, AMOUNT_STUB);
         //endregion
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowIllegalArgumentExceptionWhenAccountsIsNull() {
+    public void shouldThrowIllegalArgumentExceptionWhenAmountIsNegative() {
         //region when
-        Client sut = new Client(ID_STUB, "dummy client name", null);
+        SavingAccount sut = new SavingAccount(ID_STUB, null, -5.0);
         //endregion
     }
-
 }
