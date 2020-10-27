ae319fbb0f2ac51cb3e89874c1cbd1f0b3ab984e
Kate Esina <ekaterina.esina@raiffeisen.ru>
2020-10-27 13:35:35 +0500

    added guard clauses

--- src/main/java/com/acme/banking/dbo/domain/Client.java
+++ src/main/java/com/acme/banking/dbo/domain/Client.java
@@ -10,6 +10,8 @@ public class Client {
     private Collection<Account> accounts = new ArrayList<>(); //TODO
 
     public Client(UUID id, String name) {
+        if (id == null) throw new IllegalArgumentException("id");
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("name");
         this.id = id;
         this.name = name;
     }
--- src/main/java/com/acme/banking/dbo/domain/SavingAccount.java
+++ src/main/java/com/acme/banking/dbo/domain/SavingAccount.java
@@ -8,6 +8,9 @@ public class SavingAccount implements Account {
     private double amount;
 
     public SavingAccount(UUID id, Client client, double amount) {
+        if (id == null) throw new IllegalArgumentException("id");
+        if (client == null) throw new IllegalArgumentException("client");
+        if (amount < 0) throw new IllegalArgumentException("amount");
         this.id = id;
         this.client = client;
         this.amount = amount;
