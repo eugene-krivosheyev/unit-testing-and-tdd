6673026088a03d86b19ece0556de9fb167f57c02
Sergey Sidorov <sergey.i.sidorov@raiffeisen.ru>
2020-10-27 12:12:33 +0300

    added argumetn assertions for SavingAccoun

--- src/main/java/com/acme/banking/dbo/domain/SavingAccount.java
+++ src/main/java/com/acme/banking/dbo/domain/SavingAccount.java
@@ -3,11 +3,15 @@ package com.acme.banking.dbo.domain;
 import java.util.UUID;
 
 public class SavingAccount implements Account {
-    private UUID id;
-    private Client client;
-    private double amount;
+    private final UUID id;
+    private final Client client;
+    private final double amount;
 
     public SavingAccount(UUID id, Client client, double amount) {
+        if (id == null) throw new IllegalArgumentException("Id is null");
+        if (client == null) throw new IllegalArgumentException("Client is null");
+        if (amount < 0) throw new IllegalArgumentException("Amount is negative");
+
         this.id = id;
         this.client = client;
         this.amount = amount;
