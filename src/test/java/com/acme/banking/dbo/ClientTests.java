8b626d0451ec05adecdad8a26943302a990638e1
rualdd3 <Dmitry.LEBED@raiffeisen.ru>
2020-10-27 14:23:46 +0500

    added test client with no id

--- src/test/java/com/acme/banking/dbo/ClientTests.java
+++ src/test/java/com/acme/banking/dbo/ClientTests.java
@@ -8,14 +8,22 @@ import java.util.UUID;
 import static org.junit.Assert.*;
 
 public class ClientTests {
+    private static final UUID VALID_ID = UUID.randomUUID();
+    private static final String VALID_NAME = "TEST_NAME";
 
     @Test
     public void shouldCreateNewClientWhenValidArguments() {
-        UUID id = UUID.randomUUID();
-        String name = "TestClient";
+        Client sut = new Client(VALID_ID, VALID_NAME);
+        assertEquals(sut.getId(), VALID_ID);
+        assertEquals(sut.getName(), VALID_NAME);
+    }
 
-        Client sut = new Client(id, name);
-
-        assertEquals(sut.getName(), name);
+    @Test
+    public void shouldFailWhenCreateClientWithNullId() {
+        try {
+            new Client(VALID_ID, VALID_NAME);
+        } catch (IllegalArgumentException exception) {
+            assertNotNull(exception);
+        }
     }
 }
