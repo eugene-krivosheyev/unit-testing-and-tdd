e2af4e687d094ae616aba832162deefcb00ff969
Ekaterina Esina <esinaekwork@gmail.com>
2020-10-27 15:14:38 +0500

    Update Client.java

--- src/main/java/com/acme/banking/dbo/domain/Client.java
+++ src/main/java/com/acme/banking/dbo/domain/Client.java
@@ -10,8 +10,8 @@ public class Client {
     private Collection<Account> accounts = new ArrayList<>(); //TODO
 
     public Client(UUID id, String name) {
-        if (id == null) throw new IllegalArgumentException("id");
-        if (name == null || name.isBlank()) throw new IllegalArgumentException("name");
+        if (id == null) throw new IllegalArgumentException("id is null");
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("name is null");
         this.id = id;
         this.name = name;
     }
