1d85048d0ab353198030f2b8b59425773cd6d0c9
STEPANOV Petr <Petr.Stepanov@raiffeisen.ru>
2020-10-29 09:49:58 +0300

    New Test.

--- src/test/java/com/acme/banking/dbo/ClientTest.java
+++ src/test/java/com/acme/banking/dbo/ClientTest.java
@@ -3,12 +3,8 @@ package com.acme.banking.dbo;
 import com.acme.banking.dbo.domain.Client;
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.internal.matchers.Null;
 
 import java.util.UUID;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 import static org.junit.Assert.*;
 
 public class ClientTest {
@@ -16,55 +12,43 @@ public class ClientTest {
 
     @Test
     public void shouldClientNormallyCreatedWhenCorrectParams() {
-        //region when
         Client sut = new Client(ID_STUB, "example");
-        //endregion
-
-        //region then
 
         Assert.assertEquals(ID_STUB, sut.getId());
         Assert.assertEquals("example", sut.getName());
-        //endregion
     }
 
 
     @Test
     public void shouldNotCreateWhenIdIsNull() {
-        boolean valid = true;
         //region when
-        try
-        {
-        Client sut = new Client(null, "dummy");
-        assertFalse(true);
-        }catch (IllegalArgumentException e)
-        {
-            assertEquals("id is Null" , e.getMessage());
+        try {
+            Client sut = new Client(null, "dummy");
+            assertFalse(true);
+        } catch (IllegalArgumentException e) {
+            assertEquals("id is Null", e.getMessage());
         }
     }
 
     @Test
     public void shouldNotCreateWhenNameIsNull() {
-        boolean valid = true;
         //region when
-        try
-        {
+        try {
             Client sut = new Client(ID_STUB, null);
             assertFalse(true);
-        }catch (IllegalArgumentException e)
-        {
-            assertEquals("name is Null" , e.getMessage());
+        } catch (IllegalArgumentException e) {
+            assertEquals("name is Null", e.getMessage());
         }
     }
 
     @Test
     public void shouldNotCreateWhenNameIsEmpty() {
-        boolean valid = true;
         //region when
         try {
             Client sut = new Client(ID_STUB, "");
             assertFalse(true);
         } catch (IllegalArgumentException e) {
-            assertEquals("name is Empty" , e.getMessage());
+            assertEquals("name is Empty", e.getMessage());
         }
     }
 }
--- src/test/java/com/acme/banking/dbo/SavingAccountTest.java
+++ src/test/java/com/acme/banking/dbo/SavingAccountTest.java
@@ -1,13 +1,56 @@
 package com.acme.banking.dbo;
 
 import com.acme.banking.dbo.domain.Client;
+import com.acme.banking.dbo.domain.SavingAccount;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.UUID;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class SavingAccountTest {
 
     public static final UUID ID_STUB = UUID.fromString("8fe9595d-de6e-4d07-bc56-dacdad16f5c2");
 
+
+    @Test
+    public void shouldSavingAccountCreatedWhenCorrectParams() {
+        SavingAccount sut = new SavingAccount(ID_STUB, new Client(ID_STUB, "example"), 1);
+
+        Assert.assertEquals(ID_STUB, sut.getId());
+        Assert.assertEquals(1, sut.getAmount(), 0000);
+        Assert.assertNotNull(sut.getClient());
+    }
+
+    @Test
+    public void shouldNotCreateWhenIdIsNull() {
+        try {
+            SavingAccount sut = new SavingAccount(null, new Client(ID_STUB, "example"), 1);
+            assertFalse(true);
+        } catch (IllegalArgumentException e) {
+            assertEquals("id is Null", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldNotCreateWhenClientIsNull() {
+        try {
+            SavingAccount sut = new SavingAccount(ID_STUB, null, 1);
+            assertFalse(true);
+        } catch (IllegalArgumentException e) {
+            assertEquals("client is Null", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldNotCreateWhenAmountIsNotCorrect() {
+        try {
+            SavingAccount sut = new SavingAccount(ID_STUB, new Client(ID_STUB, "example"), -1);
+            assertFalse(true);
+        } catch (IllegalArgumentException e) {
+            assertEquals("amount is not correct", e.getMessage());
+        }
+    }
 }
