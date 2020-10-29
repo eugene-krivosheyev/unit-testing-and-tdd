c911bf1284df2f1dbd8b48b34e4e88c45996b762
STEPANOV Petr <Petr.Stepanov@raiffeisen.ru>
2020-10-29 11:03:49 +0300

    Update builder.

--- src/test/java/com/acme/banking/dbo/Factories/SavingAccountBuider.java
+++ src/test/java/com/acme/banking/dbo/Factories/SavingAccountBuider.java
@@ -8,9 +8,9 @@ import java.util.UUID;
 public class SavingAccountBuider {
     public static final UUID ID_STUB = UUID.fromString("8fe9595d-de6e-4d07-bc56-dacdad16f5c2");
 
-    private UUID _id;
-    private Client _client;
-    private double _amount;
+    private UUID _id = ID_STUB;
+    private Client _client =  new Client(ID_STUB, "example");;
+    private double _amount = 1;
 
 
     public SavingAccountBuider SetId(UUID id)
