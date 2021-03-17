83cb49e3c2d1e3b79e82cf88502fdef5fdf3c2f7
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-17 15:11:09 +0300

    Add assume demo.

--- src/test/java/demo/ArrayListTest.java
+++ src/test/java/demo/ArrayListTest.java
@@ -1,12 +1,15 @@
 package demo;
 
 import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
 
 /**
  * TestCase for SUT | Fixture
@@ -17,14 +20,17 @@ public class ArrayListTest {
      * BDD = DDD(tests)
      */
     @Test // -> config (xml, json)
+    //@Ignore | @Disable
     public void shouldSizeIncrementedAndContainsElementWhenAddElement() {
         //region Arrange | Given | Fixture
-        final ArrayList<Object> sut = new ArrayList<>();
         final Object dummy = new Object();
+        final ArrayList<Object> sut = new ArrayList<>();
+        assumeTrue(sut.isEmpty());
         //endregion
 
         //region Act | When
         sut.add(dummy);
+        //if (1==1) throw new RuntimeException("aaaaa!");
         //endregion
 
         //region Assert | Then
