52db1870727606ebb8154e070a677ac109d21b48
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-22 11:41:07 +0300

    Add assertions demo.

--- src/test/java/com/acme/banking/dbo/ClientTest.java
+++ src/test/java/com/acme/banking/dbo/ClientTest.java
@@ -1,6 +1,7 @@
 package com.acme.banking.dbo;
 
 import com.acme.banking.dbo.domain.Client;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,7 +17,7 @@ import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 
 @DisplayName("Test suite")
 public class ClientTest {
-    @Test
+    @Test @Disabled //@Ignore
     @DisplayName("Test case")
     public void shouldStorePropertiesWhenCreated() {
         //region given
@@ -31,21 +32,27 @@ public class ClientTest {
 
         //region then
         //Junit5:
-        assertAll("Client store its properties",
+        Assertions.assertNotEquals(.3, .1 + .2, "Surprise!!!");
+        Assertions.assertSame("a", "a"); // o1 == o2
+
+        //комбинатор
+        assertAll("Client successfully store its properties",
+//                noName() { assertEquals(clientId, sut.getId()) }
                 () -> assertEquals(clientId, sut.getId()),
                 () -> assertEquals(clientName, sut.getName())
         );
 
-        //Hamcrest:
+        //Hamcrest: matchers
         assertThat(sut,
-            allOf(
-                hasProperty("id", notNullValue()),
+            allOf( //anyOf
+                not(hasProperty("id", nullValue())),
                 hasProperty("id", equalTo(clientId)),
                 hasProperty("name", is(clientName))
         ));
 
-        //AssertJ:
+        //AssertJ: fluent API
         org.assertj.core.api.Assertions.assertThat(sut)
+                .isNotNull()
                 .hasFieldOrPropertyWithValue("id", clientId)
                 .hasFieldOrPropertyWithValue("name", clientName);
         //endregion
