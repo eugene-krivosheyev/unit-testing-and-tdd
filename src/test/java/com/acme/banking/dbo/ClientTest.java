0781ba193d5b04b8d78256cd04e514410bd61f84
UVAROV Aleksandr <Aleksandr.O.UVAROV@raiffeisen.ru>
2021-03-22 15:26:25 +0300

    lesson 2

--- src/test/java/com/acme/banking/dbo/ClientTest.java
+++ src/test/java/com/acme/banking/dbo/ClientTest.java
@@ -1,7 +1,7 @@
 package com.acme.banking.dbo;
 
 import com.acme.banking.dbo.domain.Client;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,6 +16,9 @@ import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 
 @DisplayName("Test suite")
 public class ClientTest {
+    private final int ID_STUB = 1;
+    private final String CLIENT_NAME = "Dummy";
+
     @Test
     @DisplayName("Test case")
     public void shouldStorePropertiesWhenCreated() {
@@ -32,22 +35,53 @@ public class ClientTest {
         //region then
         //Junit5:
         assertAll("Client store its properties",
-                () -> assertEquals(clientId, sut.getId()),
-                () -> assertEquals(clientName, sut.getName())
+                  () -> assertEquals(clientId, sut.getId()),
+                  () -> assertEquals(clientName, sut.getName())
         );
 
         //Hamcrest:
         assertThat(sut,
-            allOf(
-                hasProperty("id", notNullValue()),
-                hasProperty("id", equalTo(clientId)),
-                hasProperty("name", is(clientName))
-        ));
+                   allOf(
+                           hasProperty("id", notNullValue()),
+                           hasProperty("id", equalTo(clientId)),
+                           hasProperty("name", is(clientName))
+                   ));
 
         //AssertJ:
         org.assertj.core.api.Assertions.assertThat(sut)
-                .hasFieldOrPropertyWithValue("id", clientId)
-                .hasFieldOrPropertyWithValue("name", clientName);
+                                       .hasFieldOrPropertyWithValue("id", clientId)
+                                       .hasFieldOrPropertyWithValue("name", clientName);
         //endregion
+    }
+
+    @Test
+    public void shouldGetErrorWhenNameIsNull() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                                () -> new Client(ID_STUB, null));
+    }
+    @Test
+    public void shouldGetErrorWhenIdIsNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                                () -> new Client(-1, CLIENT_NAME));
+    }
+    @Test
+    public void shouldGetErrorWhenIdIsZero() {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                                () -> new Client(0, CLIENT_NAME));
+    }
+    @Test
+    public void shouldReturnId() {
+        Client client = new Client(ID_STUB, CLIENT_NAME);
+        int sut = client.getId();
+
+        assertEquals(ID_STUB, sut);
+    }
+
+    @Test
+    public void shouldReturnName() {
+        Client client = new Client(ID_STUB, CLIENT_NAME);
+        String sut = client.getName();
+
+        assertEquals(CLIENT_NAME, sut);
     }
 }
