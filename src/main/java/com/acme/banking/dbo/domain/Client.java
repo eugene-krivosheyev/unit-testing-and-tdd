20eddd78803be166d2789e539c7e68a2c4e570f2
Kansiandra Zhulitova <Kansiandra.ZHULITOVA@raiffeisen.ru>
2021-03-22 12:25:01 +0300

    Revert "Client and SavingAccount"
    This reverts commit 7b4e1a55

--- src/main/java/com/acme/banking/dbo/domain/Client.java
+++ src/main/java/com/acme/banking/dbo/domain/Client.java
@@ -10,10 +10,6 @@ public class Client {
     private Collection<Account> accounts = new ArrayList<>(); //TODO
 
     public Client(UUID id, String name) {
-        if (id==null) throw new IllegalArgumentException("id should not be null");
-        if (name==null) throw new IllegalArgumentException("name should not be null");
-        if (name.isEmpty()) throw new IllegalArgumentException("name should not be empty");
-        if (name.trim().isEmpty()) throw new IllegalArgumentException("name should not be only spaces");
         this.id = id;
         this.name = name;
     }
