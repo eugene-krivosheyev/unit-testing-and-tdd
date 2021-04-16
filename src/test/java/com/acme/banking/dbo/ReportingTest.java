8fb5ec90027301271b0fdc3f31388ee6153ee3e5
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-04-16 14:08:22 +0300

    Add TDD small steps demo.

--- src/test/java/com/acme/banking/dbo/ReportingTest.java
+++ src/test/java/com/acme/banking/dbo/ReportingTest.java
@@ -1,5 +1,6 @@
 package com.acme.banking.dbo;
 
+import com.acme.banking.dbo.dal.AccountRepository;
 import com.acme.banking.dbo.domain.Branch;
 import com.acme.banking.dbo.service.Reporting;
 import org.junit.jupiter.api.Test;
@@ -32,11 +33,55 @@ public class ReportingTest {
         assertEquals(
                 "# Branch #1" +
                 "## Account #1 (100.)" +
-                "### Client #1" +
-                "### Client #1" +
-                "### Client #1" +
+                "### Client #1 : name1" +
+                "### Client #1 : name2" +
+                "### Client #1 : name3" +
                 "## Account #2 (100.)",
+                "## Branch #2" +
+                "# Branch #3" +
+                "(empty)" +
                 sut.getReport(branchStub)
         );
+
+//        AccountRepository repoStub = mock();
+//        Reporting reportingService = new Reporting(repoStub);
+//        String report = reportingService.getReport();
+//        assertEquals("kjghdkfjgh", report);
+    }
+
+    @Test
+    public void shouldGetEmptyReportWhenEmptyBranchOnly() {
+        assertEquals(
+                "# Branch #1" +
+                "(empty)"
+                ,report);
+    }
+
+    @Test
+    public void shouldGetEmptyReportWhenEmptyTwoBranches() {
+        assertEquals(
+                "# Branch #1" +
+                "(empty)" +
+                "# Branch #2" +
+                "(empty)"
+                ,report);
+    }
+
+    @Test
+    public void shouldGetEmptyReportWhenEmptyBranchesHierarchy() {
+        assertEquals(
+                "# Branch #1" +
+                "## Branch #1-1" +
+                "(empty)"
+                ,report);
+    }
+
+    @Test
+    public void shouldGetEmptyReportWhenBranchOnlyWithEmptyAccountOnly() {
+        assertEquals(
+                "# Branch #1" +
+                "## Account #1 (0.)" +
+                "(empty)"
+                ,report);
     }
 }
