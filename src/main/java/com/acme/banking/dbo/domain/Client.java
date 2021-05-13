e25b9894ba98b1fad8950e14a8621cbd72281059
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-05-13 11:18:50 +0300

    Add guard clauses demo.

--- src/main/java/com/acme/banking/dbo/domain/Client.java
+++ src/main/java/com/acme/banking/dbo/domain/Client.java
@@ -9,8 +9,25 @@ public class Client {
     private Collection<Account> accounts = new ArrayList<>(); //TODO
 
     public Client(int id, String name) {
+        if (id < 0) throw new IllegalArgumentException("id!");
+        if (name != null) throw new IllegalArgumentException("name!");
+        if (name.isEmpty()) throw new IllegalArgumentException("name!");
+
         this.id = id;
         this.name = name;
+
+        //==========
+
+        if (id >= 0) {
+            if (name != null && !name.isEmpty()) {
+                this.id = id;
+                this.name = name;
+            } else {
+                throw new IllegalArgumentException("name!");
+            }
+        } else {
+            throw new IllegalArgumentException("id!");
+        }
     }
 
     public int getId() {
