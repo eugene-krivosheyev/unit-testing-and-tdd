663f88275b0937974b0c7b7520f523002acb7358
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-05-13 10:51:40 +0300

    Add assume demo.

--- src/test/java/demo/ListTest.java
+++ src/test/java/demo/ListTest.java
@@ -1,17 +1,22 @@
 package demo;
 
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class ListTest {
     @Test //BDD
+    @Disabled //@Ignore
     public void shouldSizeIncrementedAndContainsItemWhenAddItem() {
         //region Arrange | Fixture | Given
         final List<Object> sut = new ArrayList<>();
         final Object dummy = new Object();
+        assumeTrue(sut.size() == 0); //assumeTrue(sut.isEmpty());
         //endregion
 
         //region Act | When
