f84b8d1b8e0abc658001c4060678887caff53cd5
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-11-17 13:55:40 +0300

    Add auto test demo.

--- src/test/java/demo/ArrayListTest.java
+++ src/test/java/demo/ArrayListTest.java
@@ -1,10 +1,14 @@
 package demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;;
 
 /**
  * SUT
@@ -15,18 +19,22 @@ public class ArrayListTest {
      * Test, Test scenario, Test method
      * Naming: BDD
      */
-    @Test
+    @Test //@Disabled
     public void shouldStoreWhenAddElement() {
         //region 1: Fixture | Arrange | Given
-        final Object dummy = new Object();
         final Collection<Object> sut = new ArrayList<>();
+        final Object dummy = new Object();
+        assumeTrue(sut.isEmpty());
         //endregion
 
         //region 2: Act | When
+        sut.add(dummy);
         //endregion
 
         //region 3: Assert | Then
         //logic domain assertion -> assertions
+        assertTrue(sut.contains(dummy));
+        assertEquals(1, sut.size());
         //endregion
     }
 
