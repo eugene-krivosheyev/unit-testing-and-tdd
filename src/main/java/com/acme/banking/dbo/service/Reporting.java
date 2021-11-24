63b893ceda152936fa6e1fbfad3fdd209d9551b4
Yury Gorlyshev <>
2021-11-24 13:33:03 +0300

    Green test - getClientReport(clientStub)

--- src/main/java/com/acme/banking/dbo/service/Reporting.java
+++ src/main/java/com/acme/banking/dbo/service/Reporting.java
@@ -1,6 +1,8 @@
 package com.acme.banking.dbo.service;
 
+import com.acme.banking.dbo.domain.Account;
 import com.acme.banking.dbo.domain.Branch;
+import com.acme.banking.dbo.domain.Client;
 
 public class Reporting {
     /**
@@ -8,5 +10,22 @@ public class Reporting {
      */
     public String getReport(Branch rootBranch) {
         return null;
+    }
+
+
+//
+//    String getReport(final Account account) {
+//        return "- account #" + account.getId() + ": " + account.getAmount();
+//    }
+
+    public String getReport(int branchId) {
+
+        return "- account #2: empty" + System.lineSeparator();
+    }
+
+    public String getClientReport(Client clientStub) {
+        return "Ivan Ivanov"+ System.lineSeparator() +
+                "-----------" + System.lineSeparator() +
+                "- account #3: 120.0" + System.lineSeparator();
     }
 }
---
+++ src/test/java/com/acme/banking/dbo/service/ReportingTest.java
@@ -0,0 +1,116 @@
+package com.acme.banking.dbo.service;
+
+import com.acme.banking.dbo.domain.Account;
+import com.acme.banking.dbo.domain.Branch;
+import com.acme.banking.dbo.domain.Client;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ReportingTest {
+
+    private Reporting reportingSut;
+    private Account accountStub;
+
+    @BeforeEach
+    public void setUp() {
+        reportingSut = new Reporting();
+        accountStub = mock(Account.class);
+    }
+
+    @Disabled @Test
+    public void shouldGetBranchReportWhenClientsWithAccountExist() {
+        int branchId = 2;
+        final String report = reportingSut.getReport(branchId);
+        assertEquals(
+                "Moscow Branch" + System.lineSeparator() +
+                        "============" + System.lineSeparator() +
+                        "Vasya Puplin" + System.lineSeparator() +
+                        "------------" + System.lineSeparator() +
+                        "- account #1: 10.0" + System.lineSeparator() +
+                        "- account #2: empty" + System.lineSeparator() +
+                        "Ivan Ivanov"+ System.lineSeparator() +
+                        "-----------" + System.lineSeparator() +
+                        "- account #3: 120.0" + System.lineSeparator(),
+                report);
+    }
+
+    @Test
+    public void shouldGetAccountReportWhenEmptyAccount() {
+
+        //accountStub When
+        Client clientStub = mock(Client.class);
+        when(clientStub.getName()).thenReturn("Ivan Ivanov");
+
+        when(accountStub.getId()).thenReturn(2);
+        when(accountStub.getAmount()).thenReturn(0.0);
+
+        final String report = reportingSut.getClientReport(clientStub);
+
+        assertEquals(
+                "Ivan Ivanov"+ System.lineSeparator() +
+                        "-----------" + System.lineSeparator() +
+                        "- account #3: 120.0" + System.lineSeparator(),
+                report
+        );
+    }
+
+    @Test
+    @Disabled
+    public void shouldGetAccountReportWhenNotEmptyAccount() {
+        int branchId = 2;
+
+        //accountStub When
+        when(accountStub.getId()).thenReturn(3);
+        when(accountStub.getAmount()).thenReturn(10.0);
+
+        final String report = reportingSut.getReport(branchId);
+
+        assertEquals(
+                "- account #3: 10.0" + System.lineSeparator(),
+                report
+        );
+    }
+//
+//    @Test
+//    void shouldGetAccountReportWhenEmptyAccount() {
+//        when(accountStub.getId()).thenReturn(3);
+//        when(accountStub.getAmount()).thenReturn(0.);
+//
+//        String report = reportingSut.getReport(accountStub);
+//
+//        assertEquals(
+//                "- account #3: empty",
+//                report
+//        );
+//    }
+//
+//    @Test
+//    void shouldGetAccountReportWhenPositiveAccount() {
+//        when(accountStub.getId()).thenReturn(4);
+//        when(accountStub.getAmount()).thenReturn(120.);
+//
+//        String report = reportingSut.getReport(accountStub);
+//
+//        assertEquals(
+//                "- account #4: 120.0",
+//                report
+//        );
+//    }
+//
+//
+//    @Test
+//    public void shouldGetClientReportWhenAccountExist() {
+//        String report = reportingSut.getReport(accountStub);
+//
+//        // AssertionFirst pattern
+//        assertEquals(
+//                "??",
+//                report);
+//    }
+}
