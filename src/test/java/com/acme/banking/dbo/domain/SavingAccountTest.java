9a3a688943f1e486749d34f512b3a56b527f529e
ZHAVORONKOV Sergey <sergey.zhavoronkov@raiffeisen.ru>
2022-07-05 10:22:11 +0300

    feat: Add new test2

--- src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
+++ src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
@@ -2,12 +2,54 @@ package com.acme.banking.dbo.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class SavingAccountTest {
+
     @Test
-    public void shouldCreateNewAccountWhenClientIsNotNull() {
+    public void shouldGetErrorWhenInvalidId() {
+        //region given
+        int id = 0;
+        double amount = 1000;
+
+        int clientId = 1;
+        String clientName = "some name";
+        Client client = new Client(clientId, clientName);
+
+        assumeTrue(client.getId() == clientId);
+        assumeTrue(client.getName().equals(clientName));
+        //endregion
+
+        //region when
+        //endregion
+
+        //region then
+        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(id, client, amount));
+        //endregion
+    }
+
+    @Test
+    public void shouldGetErrorWhenClientIsNull() {
+        //region given
+        int id = 0;
+        double amount = 1000;
+
+        Client client = null;
+        //endregion
+
+        //region when
+        //endregion
+
+        //region then
+        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(id, client, amount));
+        //endregion
+    }
+
+    @Test
+    public void shouldCreateNewAccountWhenValidClientAndId() {
         //region given
         final int clientId = 1;
         final String clientName = "dummy client name";
@@ -17,6 +59,7 @@ class SavingAccountTest {
         Client client = new Client(clientId, clientName);
 
         assumeTrue(client.getId() == clientId);
+        assumeTrue(client.getName().equals(clientName));
         //endregion
 
         //region when
@@ -24,13 +67,10 @@ class SavingAccountTest {
         //endregion
 
         //region then
-        //Junit5:
         assertAll("Client store its properties",
                 () -> assertEquals(accountId, sut.getId()),
                 () -> assertEquals(amount, sut.getAmount()),
                 () -> assertEquals(client, sut.getClient())
         );
-
     }
-
 }
