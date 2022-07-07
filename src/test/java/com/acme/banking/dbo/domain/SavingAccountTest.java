6bae80f92c1483c6fb594134de6e039b5872e46b
ruaabcn <anna.korotkova@raiffeisen.ru>
2022-07-07 09:57:09 +0300

    Change framework

--- src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
+++ src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
@@ -2,43 +2,50 @@ package com.acme.banking.dbo.domain;
 
 import org.junit.jupiter.api.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SavingAccountTest {
 
-    private static final int CORRECT_ID =1;
+    private static final int CORRECT_ID = 1;
     private static final int INVALID_ID = -1;
     private static final int ZERO_ID = 0;
     private static final double DUMMY_AMOUNT = 12;
     private static final double ZERO_AMOUNT = 0;
     private static final double NEGATIVE_AMOUNT = -12;
-    private static final int DUMMY_ID =1;
+    private static final int DUMMY_ID = 1;
     private static final String DUMMY_NAME = "Dymmy name";
-    private static final Client DUMMY_CLIENT = new Client(DUMMY_ID,DUMMY_NAME);
-
+    private static final Client DUMMY_CLIENT = new Client(DUMMY_ID, DUMMY_NAME);
 
 
     @Test
     void shouldSaveAccount() {
         SavingAccount sut = new SavingAccount(CORRECT_ID, DUMMY_CLIENT, DUMMY_AMOUNT);
-        assertNotNull(sut);
+        org.assertj.core.api.Assertions.assertThat(sut)
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("id", 1)
+                .hasFieldOrPropertyWithValue("client", DUMMY_CLIENT)
+                .hasFieldOrPropertyWithValue("amount", DUMMY_AMOUNT);
     }
 
     @Test
     void shouldntSaveAccountWithZeroId() {
-        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(ZERO_ID, DUMMY_CLIENT, DUMMY_ID));
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> new SavingAccount(ZERO_ID, DUMMY_CLIENT, DUMMY_ID))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Id shouldn't be 0");
 
     }
 
     @Test
     void shouldntSaveAccountWithNegativeId() {
-        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(INVALID_ID, DUMMY_CLIENT, DUMMY_AMOUNT));
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> new SavingAccount(INVALID_ID, DUMMY_CLIENT, DUMMY_AMOUNT))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void shouldntSaveAccountWithNullClient() {
-        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(DUMMY_ID, null, DUMMY_AMOUNT));
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> new SavingAccount(DUMMY_ID, null, DUMMY_AMOUNT))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -48,7 +55,8 @@ class SavingAccountTest {
 
     @Test
     void shouldntSaveAccountWithZeroAmount() {
-        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(DUMMY_ID, DUMMY_CLIENT, ZERO_AMOUNT));
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> new SavingAccount(DUMMY_ID, DUMMY_CLIENT, ZERO_AMOUNT))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
