ec540a7c612bc496cf189f994acb1abe037d1568
ruavuy3 <yaroslav.vinokurov@raiffeisen.ru>
2023-03-13 15:49:38 +0300

    tests

--- src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
+++ src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
@@ -1,11 +1,39 @@
 package com.acme.banking.dbo.domain;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class SavingAccountTest {
+    Client CLIENT = new Client(200, "Ivan");
+    double AMOUNT = 100.24;
+    int ACCOUNT_ID = 100;
+
     @Test
     void shouldCreateSavingAccount() {
-        SavingAccount savingAccount = new SavingAccount(100, new Client(200, "Ivan"), 100.24);
+        SavingAccount savingAccount = new SavingAccount(ACCOUNT_ID, CLIENT, AMOUNT);
+
+        Assertions.assertEquals(ACCOUNT_ID, savingAccount.getId());
+        Assertions.assertEquals(CLIENT.getId(), savingAccount.getClient().getId());
+        Assertions.assertEquals(CLIENT.getName(), savingAccount.getClient().getName());
+        Assertions.assertEquals(AMOUNT, savingAccount.getAmount());
     }
 
+    @Test
+    void shouldThrowWhenNegativeAccountId() {
+        int accountId = -100;
+        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(accountId, CLIENT, AMOUNT));
+    }
+
+    @Test
+    void shouldThrowWhenClientIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(ACCOUNT_ID, null, AMOUNT));
+    }
+
+    @Test
+    void shouldThrowWhenNegativeAmount() {
+        double amount = -100;
+        assertThrows(IllegalArgumentException.class, () -> new SavingAccount(ACCOUNT_ID, CLIENT, amount));
+    }
 }
