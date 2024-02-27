ca541f2e8bd05f420fddc136a1a6ca8b60aaf9b4
Martirosyan Tigran <tigran.martirosyan@raiffeisen.ru>
2024-02-27 13:56:21 +0300

    [t1] guard clauses

--- src/test/java/com/acme/banking/dbo/SavingAccountTest.java
+++ src/test/java/com/acme/banking/dbo/SavingAccountTest.java
@@ -3,15 +3,24 @@ package com.acme.banking.dbo;
 import com.acme.banking.dbo.domain.Client;
 import com.acme.banking.dbo.domain.SavingAccount;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class SavingAccountTest {
     @Nested
     class Positive {
+
+        private Client client = null;
+
+        @BeforeEach
+        public void setUp() {
+            client = new Client(0, "test");
+        }
+
         @Test()
         public void shouldCreateWhenIdIsZero() {
-            Client client = new Client(0, "test");
+            client = new Client(0, "test");
             SavingAccount sut = new SavingAccount(0, client, 10);
 
             Assertions.assertEquals(0, sut.getId());
@@ -21,17 +30,17 @@ public class SavingAccountTest {
 
         @Test()
         public void shouldCreateWhenIdIsPositive() {
-            Client client = new Client(0, "test");
+            client = new Client(0, "test");
             SavingAccount sut = new SavingAccount(10, client, 10);
 
-            Assertions.assertEquals(0, sut.getId());
+            Assertions.assertEquals(10, sut.getId());
             Assertions.assertEquals(client, sut.getClient());
             Assertions.assertEquals(10, sut.getAmount());
         }
 
         @Test()
         public void shouldNotThrowWhenCorrectInput() {
-            Client client = new Client(0, "test");
+            client = new Client(0, "test");
 
             Assertions.assertDoesNotThrow(() -> new SavingAccount(0, client, 12));
         }
@@ -39,24 +48,28 @@ public class SavingAccountTest {
 
     @Nested
     class Negative {
+        private Client client = null;
+
+        @BeforeEach
+        public void setUp() {
+            client = new Client(0, "test");
+        }
+
         @Test()
         public void shouldFailWhenClientIsNull() {
+            client = null;
             Assertions.assertThrows(IllegalArgumentException.class,
                     () -> new SavingAccount(0, null, 12));
         }
 
         @Test()
         public void shouldFailWhenAmountIsNegativeIsEmpty() {
-            Client client = new Client(0, "test");
-
             Assertions.assertThrows(IllegalArgumentException.class,
                     () -> new SavingAccount(0, client, -1));
         }
 
         @Test()
         public void shouldFailWhenIdIsNegative() {
-            Client client = new Client(0, "test");
-
             Assertions.assertThrows(IllegalArgumentException.class,
                     () -> new SavingAccount(-1, client, 10));
         }
