feb8411bbe0e922642f6a9513c9c9e9e168e75d7
ruafli5 <ivan.filimonov@raiffeisen.ru>
2024-02-28 05:49:11 +0300

    Added tests of Client entity

--- src/main/java/com/acme/banking/dbo/domain/Client.java
+++ src/main/java/com/acme/banking/dbo/domain/Client.java
@@ -9,6 +9,8 @@ public class Client {
     private Collection<Account> accounts = new ArrayList<>(); //TODO
 
     public Client(int id, String name) {
+        checkClientArguments(id, name);
+
         this.id = id;
         this.name = name;
     }
@@ -19,5 +21,24 @@ public class Client {
 
     public String getName() {
         return name;
+    }
+
+    private void checkClientArguments(int id, String name) {
+        if (id < 0) throw new InternalError("ID не может быть отрицательным числом");
+        if (name == null) throw new InternalError("Name не должно быть null");
+        if (name.equals("")) throw new InternalError("Name должно содержать хотя бы 1 букву");
+        if (name.equals(" ")) throw new InternalError("Name должно содержать хотя бы 1 букву");
+        if (name.equals("-")) throw new InternalError("Name должно содержать хотя бы 1 букву");
+        if (name.indexOf(" ") == 0) throw new InternalError("Name не должно начинаться с пробела");
+        if (name.indexOf(" ") == name.length() - 1) throw new InternalError("Name не должно оканчиваться пробелом");
+        if (name.indexOf("-") == 0) throw new InternalError("Name не должно начинаться с дефиса");
+        if (name.indexOf("-") == name.length() - 1) throw new InternalError("Name не должно оканчиваться дефисом");
+
+        String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz -";
+        for (Character ch : name.toCharArray()) {
+            if (!alphabet.contains(ch.toString().toLowerCase())) {
+                throw new InternalError("Name содержит символы помимо кириллицы, латиницы, пробела и дефиса");
+            }
+        }
     }
 }
--- src/test/java/com/acme/banking/dbo/ClientTest.java
+++ src/test/java/com/acme/banking/dbo/ClientTest.java
@@ -4,14 +4,18 @@ import com.acme.banking.dbo.domain.Client;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assumptions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 
 @DisplayName("Test suite")
@@ -38,11 +42,11 @@ public class ClientTest {
 
         //Hamcrest:
         assertThat(sut,
-            allOf(
-                hasProperty("id", notNullValue()),
-                hasProperty("id", equalTo(clientId)),
-                hasProperty("name", is(clientName))
-        ));
+                allOf(
+                        hasProperty("id", notNullValue()),
+                        hasProperty("id", equalTo(clientId)),
+                        hasProperty("name", is(clientName))
+                ));
 
         //AssertJ:
         org.assertj.core.api.Assertions.assertThat(sut)
@@ -50,5 +54,146 @@ public class ClientTest {
                 .hasFieldOrPropertyWithValue("name", clientName);
         //also take a look at `extracting()` https://stackoverflow.com/a/51812188
         //endregion
+    }
+
+    @DisplayName("Валидные данные")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0, АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНн - ОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя",
+            "2147483647, ABCDEFGHIJKLMNOPQRSTUVWXYZ - abcdefghijklmnopqrstuvwxyz"})
+    public void successCreateClientWithValidData(int id, String name) {
+        new Client(id, name);
+    }
+
+    @DisplayName("Невалидные данные: в Name посторонние символы")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0, 22",
+            "0, /",
+            "0, £"})
+    public void failedCreateClientWithNameHaveNotLettersAndSpaceAndHyphen(int id, String name) {
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name содержит символы помимо кириллицы, латиницы, пробела и дефиса", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: отрицательный id")
+    public void failedCreateClientWithNegativeId() {
+        int id = -1;
+        String name = "Максим";
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("ID не может быть отрицательным числом", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: Name - null")
+    public void failedCreateClientWithNameIsNull() {
+        int id = 0;
+        String name = null;
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name не должно быть null", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: пустое Name")
+    public void failedCreateClientWithNameIsEmpty() {
+        int id = 0;
+        String name = "";
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name должно содержать хотя бы 1 букву", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: Name - только пробел")
+    public void failedCreateClientWithNameIsOnlySpace() {
+        int id = 0;
+        String name = " ";
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name должно содержать хотя бы 1 букву", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: Name - только дефис")
+    public void failedCreateClientWithNameIsOnlyHyphen() {
+        int id = 0;
+        String name = "-";
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name должно содержать хотя бы 1 букву", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: Name начинается с пробела")
+    public void failedCreateClientWithNameStartsWithSpace() {
+        int id = 0;
+        String name = " Максим";
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name не должно начинаться с пробела", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: Name оканчивается пробелом")
+    public void failedCreateClientWithNameEndsWithSpace() {
+        int id = 0;
+        String name = "Максим ";
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name не должно оканчиваться пробелом", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: Name начинается с дефиса")
+    public void failedCreateClientWithNameStartsWithHyphen() {
+        int id = 0;
+        String name = "-Максим";
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name не должно начинаться с дефиса", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
+    }
+
+    @Test
+    @DisplayName("Невалидные данные: Name оканчивается дефисом")
+    public void failedCreateClientWithNameEndsWithHyphen() {
+        int id = 0;
+        String name = "Максим-";
+        try {
+            new Client(id, name);
+        } catch (InternalError e) {
+            assertEquals("Name не должно оканчиваться дефисом", e.getMessage(),
+                    "Исключения не совпадают!");
+        }
     }
 }
