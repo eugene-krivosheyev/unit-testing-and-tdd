dadb1e8f445001cd6e0e15413375648b364a6752
ruafli5 <ivan.filimonov@raiffeisen.ru>
2024-03-04 09:49:49 +0300

    Added tests

--- src/test/java/com/acme/banking/dbo/ProcessingTest.java
+++ src/test/java/com/acme/banking/dbo/ProcessingTest.java
@@ -21,22 +21,34 @@ public class ProcessingTest {
     }
 
     @Test
-    @DisplayName("Успешно создаём клиента в Processing")
-    void successCreateNewClient() {
-        var stubElement = mock(ClientRepository.class);
-        when(stubElement.createUniqueId())
-                .thenReturn(11);
+    @DisplayName("Успешно передать день в другой аккаунт")
+    void givenTwoAccountsAndTransferAmountBetweenThey() {
+
         var mockedRepository = mock(AccountRepository.class);
-        when(mockedRepository.getAccount(1)).thenReturn(new SavingAccount(1, new Client(1, "aoa"), 1));
-        when(mockedRepository.getAccount(2)).thenReturn(new SavingAccount(2, new Client(1, "aoa"), 0));
+        SavingAccount fromAccount = new SavingAccount(1, new Client(1, "aaa"), 1);
+        SavingAccount toAccount = new SavingAccount(2, new Client(1, "bbb"), 0);
+        when(mockedRepository.getAccount(1)).thenReturn(fromAccount);
+        when(mockedRepository.getAccount(2)).thenReturn(toAccount);
 
+        processing.transfer(1, 2, 1);
 
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(0, fromAccount.getAmount()),
+                () -> Assertions.assertEquals(1, toAccount.getAmount()));
     }
 
-
     @Test
-    @DisplayName("Успешно получаем коллекцию аккаунтов по ID клиента")
-    void creatingClient() {
+    @DisplayName("Успешно вывести деньги с аккаунта")
+    void successTransfer() {
 
+        var mockedRepository = mock(AccountRepository.class);
+        SavingAccount fromAccount = new SavingAccount(1, new Client(1, "aaa"), 1);
+        Integer toAccountId = null;
+        when(mockedRepository.getAccount(1)).thenReturn(fromAccount);
+
+        processing.transfer(1, toAccountId, 1);
+
+//        Assertions.assertAll(
+//                () -> Assertions.assertEquals()),
     }
 }
